3468a25afe53bddba706b490377840ce288cf8bd
SanzharM <sanjarmanabayev@gmail.com>
2022-01-04 17:50:21 +0600

    pin page + other stuff

--- lib/pages/pin_page/pin_page.dart
+++ lib/pages/pin_page/pin_page.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:task_manager/core/app_icons.dart';
+import 'package:task_manager/core/widgets/empty_box.dart';
+import 'package:task_manager/pages/pin_page/pin_widgets.dart';
 
 class PinPage extends StatefulWidget {
   @override
@@ -7,10 +12,106 @@ class PinPage extends StatefulWidget {
 }
 
 class _PinPageState extends State<PinPage> {
+  final _localAuth = LocalAuthentication();
+  String _tempPin = '';
+  String _pin = '';
+
+  bool hasTouchId = false;
+  bool hasFaceId = false;
+
+  // ignore: unused_element
+  void _vibrate() {
+    try {
+      HapticFeedback.lightImpact();
+    } catch (e) {}
+  }
+
+  void _numberPressed(String value) {
+    if (_pin.length < 4) {
+      _pin += value;
+      setState(() {});
+    }
+    if (_pin.length == 4) {
+      // validate pins
+      print('$_tempPin == $_pin');
+    }
+  }
+
+  void _onDelete() {
+    if (_pin.isNotEmpty) {
+      _pin = _pin.substring(0, _pin.length - 1);
+      setState(() {});
+    }
+  }
+
+  void _tryLoginWithBiometrics() async {
+    try {
+      bool didAuthenticate = await _localAuth.authenticate(
+        localizedReason: 'Авторизуйтесь, чтобы пользоваться приложением',
+      );
+      if (didAuthenticate) {
+        // Send login request
+      }
+    } catch (e) {
+      print('Unable to local auth. Error: $e');
+    }
+  }
+
+  void _checkBiometrics() async {
+    if (await _localAuth.isDeviceSupported()) {
+      final _biometrics = await _localAuth.getAvailableBiometrics();
+      if (_biometrics.contains(BiometricType.fingerprint)) hasTouchId = true;
+      if (_biometrics.contains(BiometricType.face)) hasFaceId = true;
+      if (_biometrics.contains(BiometricType.iris)) hasFaceId = true;
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    _checkBiometrics();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Text('pin page'),
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            EmptyBox(height: 24),
+            hasFaceId || hasTouchId
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 24.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        if (!hasFaceId)
+                          IconButton(
+                            icon: Image.asset(AppIcons.faceId, width: 48),
+                            onPressed: () => _tryLoginWithBiometrics(),
+                          ),
+                        if (hasTouchId)
+                          IconButton(
+                            icon: Image.asset(AppIcons.touchId, width: 48),
+                            onPressed: () => _tryLoginWithBiometrics(),
+                          ),
+                      ],
+                    ),
+                  )
+                : EmptyBox(),
+            EmptyBox(height: 48),
+            PinDots(length: 4, pin: _pin),
+            EmptyBox(height: 48),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: PinNumbers(onPressed: _numberPressed, onDelete: _onDelete),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/pages/pin_page/pin_widgets.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/cupertino.dart';
+import 'package:task_manager/core/app_colors.dart';
+import 'package:task_manager/core/widgets/empty_box.dart';
+
+class PinNumber extends StatelessWidget {
+  final String number;
+  final Function onTap;
+  PinNumber({required this.number, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    if (number.isEmpty) return const EmptyBox(width: 56, height: 56);
+
+    Widget child = Text(number, style: const TextStyle(fontSize: 28));
+    if (number == 'backspace') {
+      child = const SizedBox(
+        width: 24,
+        child: Icon(CupertinoIcons.delete_left_fill),
+      );
+    }
+    return CupertinoButton(
+      padding: const EdgeInsets.all(16.0),
+      child: child,
+      onPressed: () => onTap(),
+    );
+  }
+}
+
+class PinDots extends StatelessWidget {
+  final int length;
+  final String pin;
+  PinDots({required this.length, required this.pin});
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> dots = [];
+    for (int i = 0; i < length; i++) {
+      dots.add(
+        Container(
+          constraints: BoxConstraints(minWidth: 12, minHeight: 12),
+          decoration: BoxDecoration(
+            color: pin.length > i ? AppColors.darkGrey : AppColors.transparent,
+            shape: BoxShape.circle,
+            border: Border.all(width: 2.0, color: AppColors.darkGrey),
+          ),
+        ),
+      );
+    }
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: dots,
+    );
+  }
+}
+
+class PinNumbers extends StatelessWidget {
+  final Function(String) onPressed;
+  final Function onDelete;
+  final double spaceBetween;
+
+  PinNumbers({
+    required this.onPressed,
+    required this.onDelete,
+    this.spaceBetween = 12.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            PinNumber(number: '1', onTap: () => onPressed('1')),
+            PinNumber(number: '2', onTap: () => onPressed('2')),
+            PinNumber(number: '3', onTap: () => onPressed('3')),
+          ],
+        ),
+        EmptyBox(height: spaceBetween),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            PinNumber(number: '4', onTap: () => onPressed('4')),
+            PinNumber(number: '5', onTap: () => onPressed('5')),
+            PinNumber(number: '6', onTap: () => onPressed('6')),
+          ],
+        ),
+        EmptyBox(height: spaceBetween),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            PinNumber(number: '7', onTap: () => onPressed('7')),
+            PinNumber(number: '8', onTap: () => onPressed('8')),
+            PinNumber(number: '9', onTap: () => onPressed('9')),
+          ],
+        ),
+        EmptyBox(height: spaceBetween),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            PinNumber(number: '', onTap: () => null),
+            PinNumber(number: '0', onTap: () => onPressed('0')),
+            PinNumber(number: 'backspace', onTap: () => onDelete()),
+          ],
+        ),
+      ],
+    );
+  }
+}
